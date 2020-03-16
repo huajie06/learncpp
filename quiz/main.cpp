ed55191da291ca8c21c8c928ce02b3b04c160d55
Huajie Zhang (Mike) <huajie06@gmail.com>
2020-03-15 21:43:22 -0400

    to start 1st exercise

---
+++ quiz/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  quiz
+//
+//  Created by Huajie Zhang on 3/15/20.
+//  Copyright © 2020 Huajie Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  for (int i =0;i<10;i++){
+    std::cout << "Hello, World!\n";
+  };
+  return 0;
+}
